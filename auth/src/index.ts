a151574be81d51d8c33a0388ba8d3ebd346c39ed
certifiedTboy <etosin70@gmail.com>
2025-04-02 12:14:00 +0100

    add changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const startServer = async () => {
+  console.log("starting up auth service...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
